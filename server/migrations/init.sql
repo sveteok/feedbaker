23ec86b47292ead10f61c7b97e4dcef32e749a95
SvetlanaTeryaeva <sveteok@gmail.com>
2025-11-13 21:40:22 +0200

    Change for production

--- server/migrations/init.sql
+++ server/migrations/init.sql
@@ -1,4 +1,5 @@
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 CREATE TABLE IF NOT EXISTS "users" (
     "user_id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
