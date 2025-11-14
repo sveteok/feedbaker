f11388dec9d41de2646a4f6f50d9880e97bdad2b
SvetlanaTeryaeva <sveteok@gmail.com>
2025-11-14 22:10:02 +0200

    fix cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.options("*", restrictedCors);
+app.options("/api/*", restrictedCors);
 app.use("/api/auth", restrictedCors, authRouter);
 app.use("/api/profile", restrictedCors, profileRouter);
 app.use("/api/sites", publicCors, sitesRouter);
