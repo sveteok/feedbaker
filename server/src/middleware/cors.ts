81dc866dab59b5c11134395cdfa82a8d64e0bcb5
SvetlanaTeryaeva <sveteok@gmail.com>
2025-11-14 21:44:41 +0200

    fix cors

--- server/src/middleware/cors.ts
+++ server/src/middleware/cors.ts
@@ -4,12 +4,15 @@ export const publicCors = cors({
   origin: "*",
   methods: ["GET", "POST", "OPTIONS"],
 });
+
+const allowedOrigins = process.env.PRIVATE_CORS_ORIGINS?.split(",") || [];
+
 export const restrictedCors = cors({
   // origin: process.env.PRIVATE_CORS_ORIGINS,
   origin: function (origin, callback) {
     if (!origin) return callback(null, true);
 
-    if (process.env.PRIVATE_CORS_ORIGINS!.includes(origin)) {
+    if (allowedOrigins.includes(origin)) {
       return callback(null, true);
     } else {
       return callback(new Error("Not allowed by CORS"));
