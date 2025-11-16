5a1ec22be9fd8d9d20050c36d060b0ac364c71ee
SvetlanaTeryaeva <sveteok@gmail.com>
2025-11-16 11:37:38 +0200

    cors allowedOrigins added

--- server/src/middleware/cors.ts
+++ server/src/middleware/cors.ts
@@ -4,8 +4,19 @@ export const publicCors = cors({
   origin: "*",
   methods: ["GET", "POST", "OPTIONS"],
 });
+
+const allowedOrigins = process.env.PRIVATE_CORS_ORIGINS?.split(",") || [];
+
 export const restrictedCors = cors({
-  origin: process.env.PRIVATE_CORS_ORIGINS,
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 });
